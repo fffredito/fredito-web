88bc38d361d1b28af1d2da6fd3f2c0662b4b83e1
fffredito <92886748+fffredito@users.noreply.github.com>
2023-09-06 14:13:28 -0400

    d

--- scripts/script.js
+++ scripts/script.js
@@ -117,3 +117,25 @@ function nextQuestion() {
 
 // don't forget to initiate your game!!
 // EXAMPLES OF CODE
+
+var opacity = 0;
+var intervalID = 0;
+window.onload = fadeIn;
+  
+function fadeIn() {
+    setInterval(show, 200);
+}
+  
+function show() {
+    var body = document.getElementById("body");
+    opacity = Number(window.getComputedStyle(body)
+                    .getPropertyValue("opacity"));
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        body.style.opacity = opacity
+    } else {
+        clearInterval(intervalID);
+    }
+}
+
+fadeIn();
